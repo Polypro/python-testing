656f7e82c6c8c8fc058194b11c07d8e7bc7fb593
Polypro <davidpotvin5@gmail.com>
2020-06-04 12:30:05 -0400

    Invinting persons to a party

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def x(msg):
-    print(msg)
-    return 1
-
-x("Hello world")
-print("How dare you")
+guest_list = ["Rob", "Joe", "Gavin", "Peter"]
+# 0 : guest accepted the invitation
+# 1 : guest cannot make it
+guest_list_status = [0, 0, 1, 0]
+for x in range(len(guest_list)):
+    if guest_list_status[x] == 0:
+        print(guest_list[x],  "will come to the party")
+    else:
+        print(guest_list[x], "cannot make it to the party")
