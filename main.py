8cd5567ee45f057992982f96875ace7a91dfddda
Polypro <davidpotvin5@gmail.com>
2020-06-05 14:34:02 -0400

    Creating function that creater user prfile

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-import math
-guest_list = ["Rob", "Joe", "Gavin", "Peter", "X", "DMX"]
-guest_substitute = ["Caitlyn", "Jenna", "Ford"]
-# 0 : guest accepted the invitation
-# 1 : guest cannot make it
-guest_list_status = [0, 0, 1, 0, 1, 0]
-n_absent = sum(guest_list_status)
-n_substitute = len(guest_substitute)
-absent = 0
-for x in range(len(guest_list)):
-    if guest_list_status[x] == 0:
-        print(guest_list[x], "will come to the party")
-    else:
-        print(guest_list[x], "cannot make it to the party so", guest_substitute[absent], "will replace this guest")
-        guest_list.pop(x)
-        guest_list.append(guest_substitute[absent])
-        absent+=1
+def user(first_name, last_name, **informations):
+    profile = {}
+    profile["first_name"] = first_name
+    profile["last_name"] = last_name
 
-print("\n final list :", guest_list)
+    for k, v in informations.items():
+        profile[k] = v
+
+    return profile
+
+def print_profile(profile):
+    for k,v in profile.items():
+        print(k, ":", v)
+
+profilex = user("david", "potvin", eye_color="blue", hair_color="red")
+print_profile(profilex)
